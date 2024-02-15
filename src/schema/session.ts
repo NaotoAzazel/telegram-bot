c09835daa807a0ec04ec393e0e4a7685149cb287
Igor <87926186+NaotoAzazel@users.noreply.github.com>
2024-02-15 21:44:04 +0300

    Changed defaul value

--- src/schema/session.ts
+++ src/schema/session.ts
@@ -1,12 +1,14 @@
 import mongoose, { Schema } from "mongoose";
 import { SessionData } from "../context/context.interface";
 
+const year = new Date().getFullYear();
+
 const SessionModel = new Schema<SessionData>({
   id: { type: String, required: true },
-  startYear: { type: String, default: "" },
-  endYear: { type: String, default: "" },
-  minRating: { type: String, default: "" },
-  maxRating: { type: String, default: "" },
+  startYear: { type: String, default: "1975" },
+  endYear: { type: String, default: year },
+  minRating: { type: String, default: "5" },
+  maxRating: { type: String, default: "10" },
   genre: { type: String, default: "" },
   type: { type: String, default: "" }
 }, { timestamps: true });
