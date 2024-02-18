99984565a50dc824eee5492f45c616a7163d8470
Igor <87926186+NaotoAzazel@users.noreply.github.com>
2024-02-18 23:36:20 +0300

    fixed error when trying to change main message

---
+++ src/commands/button.event.ts
@@ -0,0 +1,31 @@
+import { Telegraf } from "telegraf";
+import { Command } from "./command.class";
+import { IBotContext } from "../context/context.interface";
+import { ISessionService } from "../service/session.interface";
+import Menu from "../config/menu.class";
+
+export default class ButtonEvent extends Command {
+  constructor(bot: Telegraf<IBotContext>, session: ISessionService) { 
+    super(bot, session);
+  }
+
+  handle(): void {
+    this.bot.on("callback_query", async(ctx, next) => {
+      const mainMessage = this.session?.getMainMessage();
+      const callbackQuery = ctx.callbackQuery;
+      const errorMenuText = Menu.createErrorMenu();
+
+      if(callbackQuery && mainMessage!.messageId < 0) {
+        ctx.telegram.editMessageText(
+          callbackQuery.message?.chat.id, 
+          callbackQuery.message?.message_id,
+          undefined,
+          errorMenuText
+        )
+
+        return false;
+      }
+      next();
+    });
+  }
+}
