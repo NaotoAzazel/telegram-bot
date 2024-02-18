5a6aedee9d56ee02ed796a357e036c849d290c37
Igor <87926186+NaotoAzazel@users.noreply.github.com>
2024-02-18 23:31:38 +0300

    update default value

--- src/schema/session.ts
+++ src/schema/session.ts
@@ -1,12 +1,10 @@
 import mongoose, { Schema } from "mongoose";
 import { SessionData } from "../context/context.interface";
 
-const year = new Date().getFullYear();
-
 const SessionModel = new Schema<SessionData>({
   id: { type: String, required: true },
   startYear: { type: String, default: "1975" },
-  endYear: { type: String, default: year },
+  endYear: { type: String, default: "2020" },
   minRating: { type: String, default: "5" },
   maxRating: { type: String, default: "10" },
   genre: { type: String, default: "" },
