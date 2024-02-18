abd102164911e7f3509ed3d48383b27f5fd4b2db
Igor <87926186+NaotoAzazel@users.noreply.github.com>
2024-02-18 23:34:26 +0300

    added functions to interact with session

--- src/service/session.interface.ts
+++ src/service/session.interface.ts
@@ -1,6 +1,12 @@
 import { SessionData } from "../context/context.interface";
+import { MainMessage } from "./session.service";
 
 export interface ISessionService {
   create(id: string): Promise<void>;
-  findById(id: string): Promise<SessionData | null>
+  findById(id: string): Promise<SessionData | null>;
+  updateById(field: keyof SessionData, value: string, id: string): Promise<void>;
+  setLastMessageId(messageId: number): void;
+  getLastMessageId(): number;
+  setMainMessage({ messageId, chatId }: MainMessage): void;
+  getMainMessage(): MainMessage;
 }
--- src/service/session.service.ts
+++ src/service/session.service.ts
@@ -2,11 +2,16 @@ import { SessionData } from "../context/context.interface";
 import SessionModel from "../schema/session";
 import { ISessionService } from "./session.interface";
 
+export type MainMessage = {
+  messageId: number;
+  chatId: number;
+}
+
 export class SessionService implements ISessionService {
+  private lastMessageId: number = -1;
+  private mainMessage: MainMessage = { messageId: -1, chatId: -1 };
+
   async create(id: string): Promise<void> {
-    if(!id) {
-      throw new Error("No id specified");
-    }
     try {
       await SessionModel.create({ id });
     } catch(err) {
@@ -15,14 +20,36 @@ export class SessionService implements ISessionService {
   }
 
   async findById(id: string): Promise<SessionData | null> {
-    if(!id) {
-      throw new Error("No id specified");
-    }
     try {
       const session = await SessionModel.findOne({ id });
       return session;
     } catch(err) {
       throw new Error(err as string);
     }
+  }
+
+  async updateById(field: string, value: string, id: string) {
+    try {
+      const update = { [field]: value };
+      await SessionModel.findOneAndUpdate({ id }, update, { new: true });
+    } catch(err) {
+      throw new Error(err as string);
+    }
+  }
+
+  setLastMessageId(messageId: number) {
+    this.lastMessageId = messageId;
+  }
+
+  getLastMessageId() {
+    return this.lastMessageId;
+  }
+
+  setMainMessage({ messageId, chatId }: MainMessage) {
+    this.mainMessage = { messageId, chatId };
+  }
+
+  getMainMessage(): MainMessage {
+    return this.mainMessage;
   }
 }
