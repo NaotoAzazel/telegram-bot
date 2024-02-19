0f97f158088c37786f0027acd1428395b1b770f5
Igor <87926186+NaotoAzazel@users.noreply.github.com>
2024-02-19 21:54:21 +0300

    added filter action processing

--- src/commands/inline.event.ts
+++ src/commands/inline.event.ts
@@ -1,23 +1,127 @@
 import { Telegraf } from "telegraf";
 import { Command } from "./command.class";
-import { IBotContext } from "../context/context.interface";
+import { IBotContext, SessionData } from "../context/context.interface";
 import { ISessionService } from "../service/session.interface";
+import { InlineQueryResultArticle } from "telegraf/typings/core/types/typegram";
+import { generateNumberInlineQuery, generateTextInlineQuery } from "../utils";
+import { GENRES, TYPES } from "../config/genresAndTypes.constants";
 
 export default class InlineEvent extends Command {
   constructor(bot: Telegraf<IBotContext>, session: ISessionService) { 
-    super(bot);
-    this.session = session;
+    super(bot, session);
   }
 
   handle(): void {
-    this.bot.on("inline_query", async(ctx) => {
-      const filterType = ctx.inlineQuery.query.slice(7);
-      
-      // TODO: added logic for each button from filter menu
+    this.bot.on("message", (ctx, next) => {
+      this.session?.setLastMessageId(ctx.message.message_id);
+      next();
+    });
+
+    this.bot.on("chosen_inline_result", async(ctx) => {
+      const userId = ctx.from.id;
+      const chosenResult = ctx.chosenInlineResult;
+      const mainMessage = this.session.getMainMessage();
+      const lastMessageId = this.session.getLastMessageId();
+
+      if(mainMessage.messageId < 0) return;
+
+      let session = await this.session.findById(userId);
+      if(!session) {
+        throw new Error("Session not found")
+      }
+
+      const filterType = chosenResult.query.slice(7);
+      const choseResult = ctx.chosenInlineResult;
+
       switch(filterType) {
         case "minRating": {
+          if(Number(choseResult.result_id) > Number(session.maxRating)) {
+            await this.session.updateById("maxRating", choseResult.result_id, userId);
+          }
+          
+          await this.session.updateById(filterType, choseResult.result_id, userId);
+          break;
+        }
+
+        case "maxRating": {
+          if(Number(choseResult.result_id) < Number(session.minRating)) {
+            await this.session.updateById("minRating", choseResult.result_id, userId);
+          }
+          break;
+        }
+
+        case "startYear": {
+          if(Number(choseResult.result_id) > Number(session.endYear)) {
+            await this.session.updateById("endYear", choseResult.result_id, userId);
+          }
+          break;
+        }
+
+        case "endYear": {
+          if(Number(choseResult.result_id) < Number(session.startYear)) {
+            await this.session.updateById("startYear", choseResult.result_id, userId);
+          }
+          break;
+        }
+
+        case "genre": {
+          if(choseResult.result_id === "All") {
+            choseResult.result_id = "";
+            break;
+          }
         }
       }
+
+      await this.session.updateById(filterType as keyof SessionData, choseResult.result_id, userId);
+
+      const uniqueNumber: number = new Date().getTime();
+      this.bot.handleUpdate({
+        update_id: uniqueNumber,
+        callback_query: {
+          id: uniqueNumber.toString(),
+          from: { 
+            id: userId, 
+            is_bot: false, 
+            first_name: "firtsName" 
+          }, 
+          message: undefined,
+          chat_instance: uniqueNumber.toString(),
+          data: "filter"
+        }
+      });
+      
+      if(lastMessageId) { // fix when deleting message that contain error message
+        ctx.telegram.deleteMessage(ctx.from.id, lastMessageId);
+      }
+    })
+
+    this.bot.on("inline_query", async(ctx) => {
+      const userId = ctx.from.id;
+      const filterType = ctx.inlineQuery.query.slice(7);
+      
+      let results: InlineQueryResultArticle[] = [];
+      switch(filterType) {
+        case "minRating":
+        case "maxRating": {
+          results = await generateNumberInlineQuery(0.2, 10, 0.2, userId, filterType, this.session);
+          break;
+        } 
+
+        case "startYear":
+        case "endYear": {
+          results = await generateNumberInlineQuery(1975, 2020, 1, userId, filterType, this.session);
+          break;
+        }
+
+        case "genre":
+        case "type": {
+          results = 
+            await generateTextInlineQuery(userId, filterType, filterType === "genre" ? GENRES : TYPES, this.session);
+          break;
+        }
+      }
+      
+      ctx.answerInlineQuery(results);
     })
   }
 }
---
+++ src/config/genresAndTypes.constants.ts
@@ -0,0 +1,25 @@
+export type KeyValueItem = {
+  [key: string]: string;
+}
+
+export const GENRES: KeyValueItem = {
+  "Все": "All",
+  "Экшен": "Action",
+  "Комедия": "Comedy",
+  "Триллер": "Thriller",
+  "Приключения": "Adventure",
+  "Анимация": "Animation",
+  "Научная фантастика": "Sci-fi",
+  "Ужасы": "Horror",
+  "Драма": "Drama",
+  "История": "History",
+  "Мистика": "Mystery",
+  "Спорт": "Sport",
+  "Фэнтези": "Fantasy",
+  "Семейный": "Family",
+  "Криминал": "Crime"
+};
+
+export const TYPES: KeyValueItem = {
+  "Фильм": "movie"
+};
