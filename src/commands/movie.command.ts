3ea6219540b61c0ee277dd8bcb4c8c9e58623501
Igor <87926186+NaotoAzazel@users.noreply.github.com>
2024-02-26 19:50:20 +0300

    added movie menu

---
+++ src/commands/movie.command.ts
@@ -0,0 +1,45 @@
+import { Markup, Telegraf } from "telegraf";
+import { Command } from "./command.class";
+import { IBotContext } from "../context/context.interface";
+import { ISessionService } from "../service/session.interface";
+import { BUTTONS, ButtonItem } from "../config/ui-config.constants";
+import { IDatabase } from "../service/database.interface";
+import { MovieApiService } from "../service/movie-api.service";
+import { IMovieApi, MovieDetail } from "../service/movie-api.interface";
+import Menu from "../config/menu.class";
+
+export default class MovieCommand extends Command {
+  constructor(bot: Telegraf<IBotContext>, session: ISessionService, database: IDatabase) { 
+    super(bot, session, database);
+  }
+
+  handle(): void {
+    this.bot.action("movie", async(ctx) => {
+      const baseImageUrl = "https://image.tmdb.org/t/p/w500/";
+      const mainMessage = this.session.getMainMessage();
+      const buttons = (BUTTONS.movieMenu.buttons as ButtonItem[]).map(button => 
+        Markup.button.callback(button.name, button.value));
+
+      const apiService: IMovieApi = new MovieApiService();
+      const movie: MovieDetail = await apiService.searchById(ctx.callbackQuery.id.slice(9));
+
+      const movieMenuText = Menu.createMovieMenu(movie);
+      await ctx.telegram.deleteMessage(mainMessage.chatId, mainMessage.messageId);
+      
+      const newMainMessage = await ctx.telegram.sendPhoto(
+        mainMessage.chatId,
+        baseImageUrl + movie.poster_path,
+        { caption: movieMenuText, parse_mode: "HTML" }
+      );
+
+      this.session.setMainMessage({ 
+        messageId: newMainMessage.message_id, 
+        chatId: newMainMessage.chat.id 
+      });
+
+      await ctx.telegram.editMessageReplyMarkup(mainMessage.chatId, mainMessage.messageId, undefined,
+        Markup.inlineKeyboard(buttons, { columns: 2 }).reply_markup
+      )
+    })
+  }
+}
